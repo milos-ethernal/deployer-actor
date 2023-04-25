4d2c1422cc0cfe78dc12e48bb9bf511158de4bc0
milos-ethernal <milos.maksimovic@ethernal.tech>
2023-04-25 10:06:32 +0200

    test error update

--- testing/integration/tests/deployer_test.rs
+++ testing/integration/tests/deployer_test.rs
@@ -47,8 +47,26 @@ fn deployer_working_test() {
     common::deploy_actor(actor_address, RawBytes::default(), &mut tester);
 
     // Test fails here with
-    // ExitCode { value: 24 } unexpected error: illegal operation
-    // on first send_simple call in actors method
+    // ExitCode { value: 10 }
+    // Some(MessageBacktrace(Backtrace 
+    // { frames: 
+    //     [
+    //     Frame { 
+    //         source: 1,
+    //         method: 4,
+    //         code: ExitCode { value: 10 },
+    //         message: "fatal error" 
+    //             },
+    //     Frame { 
+    //         source: 10000,
+    //         method: 4266815605,
+    //         code: ExitCode { value: 10 },
+    //         message: "fatal error" 
+    //         }
+    //     ], 
+    // cause: Some(Fatal { 
+    //     error_msg: "[from=f1d3nehuc4u3l5mn7hazppnogf3oe6l6ymaicbkhi, to=f010000, seq=0, m=4266815605, h=0]: 
+    //     unknown import: `actor::install_actor` has not been defined" })
     common::send_message(
         sender,
         actor_address,
